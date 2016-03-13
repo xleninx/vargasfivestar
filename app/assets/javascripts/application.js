e6a943baf57cb30e48a2f3c8116608ec38842649
Lenin Luque <xleninx@gmail.com>
2016-03-13 02:08:11 -0300

    new template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery-2.1.4.min.js
+//= require jquery.isotope.js
+//= require masonry.js
+//= require bootstrap/js/bootstrap.min.js
+//= require magnific-popup/jquery.magnific-popup.min.js
+//= require owl-carousel/owl.carousel.min.js
+//= require knob/js/jquery.knob.js
+//= require flexslider/jquery.flexslider-min.js
+//= require revolution-slider/js/jquery.themepunch.tools.min.js
+//= require revolution-slider/js/jquery.themepunch.revolution.min.js
+//= require revolution_slider.js
+//= require styleswitcher/styleswitcher.js
+//= require scripts.js
